b0a30a82fd4bf069b4f0d12fb33bb30ac41c43bc
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-01-11 16:40:43 +0100

    index.js
    Налаштовано початковий імпорт

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+
+import * as addToLibrary from './js/add-to-library';
+import * as buttonUp from './js/button_up';
+import * as fechLibrary from './js/fech-library';
+import * as fechQuery from './js/fech-query';
+import * as fechTrends from './js/fech-trend';
+import * as loader from './js/loader';
+import * as modalFilm from './js/modal-film';
+import * as registration from './js/modal-registared';
+import * as modalTeam from './js/modal-team';
+import * as pagination from './js/pagination';
+import * as queue from './js/queue';
+import * as watched from './js/watched';
+import * as fechFilm from './js/fech-film/fech-film';
