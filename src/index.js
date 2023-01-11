524d703287f057abfd9dbae323074c8954687162
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-01-11 11:42:02 +0100

    Pagination
    import Pagination from 'tui-pagination'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
