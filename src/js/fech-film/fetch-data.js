7cef1049c998b9eb17a3a030ef0e31bfed5d1350
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-01-11 18:37:58 +0200

    start files

---
+++ src/js/fech-film/fetch-data.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '32213066-8c27353e21dcb9ee734ab239c';
+
+export default class FetchData {
+  constructor() {
+    this.searchQuery = '';
+    this.perPage = 40;
+    this.page = 1;
+  }
+
+  async makesRequest() {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: this.pageAmount,
+      per_page: this.perPage,
+    });
+    const url = `${BASE_URL}/?${params}`;
+    this.incrementPage();
+    return await axios.get(url);
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQueary = newQuery;
+  }
+}
