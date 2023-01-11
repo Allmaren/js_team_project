57b57aecc287668b7e8d3183f5e53450bb0f4c65
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-01-11 20:04:24 +0200

    work in progress

--- src/js/fech-film/fetch-data.js
+++ src/js/fech-film/fetch-data.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api';
+// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = '32213066-8c27353e21dcb9ee734ab239c';
 
 export default class FetchData {
   constructor() {
     this.searchQuery = '';
-    this.perPage = 40;
+    this.perPage = 20;
     this.page = 1;
   }
 
   async makesRequest() {
     const params = new URLSearchParams({
-      key: API_KEY,
+      api_key: API_KEY,
       q: this.searchQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
+      language: 'en-US',
+      include_adult: false,
       page: this.pageAmount,
       per_page: this.perPage,
     });
-    const url = `${BASE_URL}/?${params}`;
+    const url = `${BASE_URL}?${params}`;
     this.incrementPage();
     return await axios.get(url);
   }
