0137689a04280fbc05f7816b5b8c52428b908a80
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-01-12 22:14:34 +0100

    v3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './js/queue';
 import './js/watched';
 
 import './js/modal-film';
-import './js/modal-register';
+import './js/modal-registered';
 import './js/modal-team';
 
 import './js/render';
