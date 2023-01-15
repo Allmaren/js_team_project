1ae63f60ea1d2be584b74a5f4117aa27dfa95d76
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-01-15 21:14:45 +0100

    fix library

---
+++ src/library.js
@@ -0,0 +1,14 @@
+import './js/fetch-library';
+import './js/fetch-query';
+
+import './js/add-to-library';
+
+import './js/pagination';
+import './js/queue';
+import './js/watched';
+
+import './js/modal-team';
+
+import './js/render';
+import './js/loader';
+// import './js/button_up';
