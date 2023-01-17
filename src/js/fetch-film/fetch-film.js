0b9690d2ecfd217195b020f56ed868d3bc185bf7
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-01-17 20:43:30 +0200

    deleting spinner btn

--- src/js/fetch-film/fetch-film.js
+++ src/js/fetch-film/fetch-film.js
@@ -1,4 +1,3 @@
-// import API from '../fetch-film/fetch-data';
 import renderCard from '../render';
 import Pagination from 'tui-pagination';
 
@@ -7,10 +6,9 @@ const API_KEY = '57c452bac8b733cbb049921c3a15e368';
 
 const searchForm = document.querySelector('.header__form');
 const gallery = document.querySelector('.search-film__by-name-js');
-const headerSearchContainer = document.querySelector('.header__search-cont');
+// const headerSearchContainer = document.querySelector('.header__search-cont');
 const searchQuery = document.querySelector('.search-film__input');
 const warningText = document.querySelector('.header__warning-text-js');
-
 
 const container = document.getElementById('pagination');
 const paginatorOptions = {
@@ -29,80 +27,42 @@ function searchHandler(event) {
   if (inputQuery === '') {
     warningText.classList.remove('is-hidden-warn');
   } else {
-    fetchData(searchQuery)
-    .then(() => {
+    fetchData(searchQuery).then(() => {
       new Pagination(container, paginatorOptions).on(
         'afterMove',
         function (eventData) {
           console.log('The current page is ' + eventData.page);
-          fetchData(searchQuery, eventData.page)
+          fetchData(searchQuery, eventData.page);
           window.scrollTo({ top: 0, behavior: 'smooth' });
         }
       );
-    }) 
+    });
   }
-};
+}
 
 function inputHandler(event) {
   // const deletingWarning = document.querySelector('.header__warning-text-js');
   warningText.classList.add('is-hidden-warn');
 }
 
-
-function fetchData(searchQuery,page = 1) {
+function fetchData(searchQuery, page = 1) {
   const params = `api_key=${API_KEY}&query='${searchQuery.value}'&language=en-US&page=${page}&include_adult=false`;
   const url = `${BASE_URL}?${params}`;
 
-  return fetch(url).then(response => response.json()).then(data => {
-    paginatorOptions.totalItems = data.total_results;
-    return data.results;
-  }).then(films => {
-    if (films.length >= 1) {
-      renderCard(films, gallery)
-      warningText.classList.add('is-hidden-warn');
-    } else {
-      warningText.classList.remove('is-hidden-warn');
-      // gallery.innerHTML = '';
-    }
-  }).catch(error => console.log(error));
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      paginatorOptions.totalItems = data.total_results;
+      return data.results;
+    })
+    .then(films => {
+      if (films.length >= 1) {
+        renderCard(films, gallery);
+        warningText.classList.add('is-hidden-warn');
+      } else {
+        warningText.classList.remove('is-hidden-warn');
+        // gallery.innerHTML = '';
+      }
+    })
+    .catch(error => console.log(error));
 }
-  
-
-// import API from '../fetch-film/fetch-data';
-// import renderCard from '../render';
-
-// const searchForm = document.querySelector('.header__form');
-// const gallery = document.querySelector('.search-film__by-name-js');
-// const headerSearchContainer = document.querySelector('.header__search-cont');
-// const searchQuery = document.querySelector('.search-film__input');
-// const warningText = document.querySelector('.header__warning-text-js');
-
-// // console.log(searchQuery.value);
-
-// searchForm.addEventListener('submit', searchHandler);
-// searchQuery.addEventListener('click', inputHandler);
-
-// function searchHandler(event) {
-//   event.preventDefault();
-
-//   const inputQuery = searchQuery.value;
-
-//   if (inputQuery === '') {
-//     warningText.classList.remove('is-hidden-warn');
-//   } else {
-//     API.fetchData(searchQuery)
-//       .then(data => data.results)
-//       .then(films => {
-//         if (films.length >= 1) {
-//           gallery.innerHTML = '';
-//           renderCard(films, gallery).catch(error => console.log(error));
-//         } else {
-//           warningText.classList.remove('is-hidden-warn');
-//         }
-//       });
-//   }
-// }
-// function inputHandler(event) {
-//   const deletingWarning = document.querySelector('.header__warning-text-js');
-//   deletingWarning.classList.add('is-hidden-warn');
-// }
--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,29 +1,3 @@
-// import { Spinner } from 'spin.js';
-
-// var opts = {
-//   lines: 11, // The number of lines to draw
-//   length: 34, // The length of each line
-//   width: 13, // The line thickness
-//   radius: 30, // The radius of the inner circle
-//   scale: 1, // Scales overall size of the spinner
-//   corners: 1, // Corner roundness (0..1)
-//   speed: 1, // Rounds per second
-//   rotate: 41, // The rotation offset
-//   animation: 'spinner-line-fade-default', // The CSS animation name for the lines
-//   direction: 1, // 1: clockwise, -1: counterclockwise
-//   color: '#9e9e9e', // CSS color or array of colors
-//   fadeColor: 'transparent', // CSS color or array of colors
-//   top: '50%', // Top position relative to parent
-//   left: '50%', // Left position relative to parent
-//   shadow: '0 0 1px transparent', // Box-shadow for the lines
-//   zIndex: 2000000000, // The z-index (defaults to 2e9)
-//   className: 'spinner', // The CSS class to assign to the spinner
-//   position: 'absolute', // Element positioning
-// };
-
-// var target = document.getElementById('foo');
-// var spinner = new Spinner(opts).spin(target);
-
 window.onload = spinner();
 
 export default function spinner() {
@@ -34,47 +8,3 @@ export default function spinner() {
   }, 500);
 }
 
-const spinnerBtn = document.querySelector('.spinner-btn-js');
-
-spinnerBtn.addEventListener('click', clickBtnHandler);
-console.log(spinnerBtn);
-
-function clickBtnHandler(event) {
-  setTimeout(() => {
-    event.target.classList.remove('spinner-btn-js');
-  }, 1500);
-  event.target.classList.add('spinner-btn-js');
-}
-
-// async function sendForm() {
-//   try {
-//     document.forms.user.querySelector('[type="submit"]').disabled = true;
-//     console.log(document.forms.user);
-//     document.forms.user
-//       .querySelector('.submit-spinner')
-//       .classList.remove('submit-spinner_hide');
-//     let response = await fetch(document.forms.user.action, {
-//       method: 'post',
-//       body: new FormData(document.forms.user),
-//     });
-//     document.forms.user.querySelector('[type="submit"]').disabled = false;
-//     document.forms.user
-//       .querySelector('.submit-spinner')
-//       .classList.add('submit-spinner_hide');
-//     if (response.ok) {
-//       let result = await response.json();
-//     }
-//   } catch (error) {
-//     document.forms.user.querySelector('[type="submit"]').disabled = false;
-//     document.forms.user
-//       .querySelector('.submit-spinner')
-//       .classList.add('submit-spinner_hide');
-//     console.log(error);
-//   }
-// }
-
-// sendForm();
-// document.forms.user.addEventListener('submit', e => {
-//   e.preventDefault();
-//   sendForm();
-// });
