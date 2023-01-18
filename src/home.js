87a69712023a782f41e3b691e617de27e89a1579
Yevheniia Bohdan <AlmDomini@gmail.com>
2023-01-18 23:21:52 +0100

    delete trash

--- src/home.js
+++ src/home.js
@@ -1,19 +1,11 @@
 import './js/spinner';
 import './js/fetch-film/fetch-film';
 import './js/fetch-trend';
-// import './js/library';
-
 import './js/pagination';
-import './js/queue';
 import './js/watched';
-
 import './js/modal-film';
 import './js/authentication';
 import './js/modal-team';
-
 import './js/render';
-import './js/loader';
 import './js/dark-mode';
-// import './js/button_up';
-
 import './js/scroll-up';
--- src/library.js
+++ src/library.js
@@ -1,21 +1,10 @@
 import './js/library';
 import './js/modal-film';
-import './js/queue';
 import './js/watched';
-
 import './js/modal-team';
 import './js/authentication';
-
-
 import './js/render';
-import './js/loader';
 import './js/dark-mode';
-// import './js/button_up';
-
-
 import './js/scroll-up';
-
 import './js/spinner';
-
 import './js/pagination';
-
